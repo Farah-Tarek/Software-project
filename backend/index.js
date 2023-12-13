6f406639f11d6d0291ed157a44c1bcdac9578763
LandyPK <100614589+LandyPK@users.noreply.github.com>
2023-12-13 21:06:37 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,16 @@ app.use(express.json());
 const userSchema = require('./models/userSchema'); // Import the User schema
 const ticketRoutes = require('./routes/ticketRoutes');  
 const agentRoutes = require('./routes/agentRoutes');  
-
+const KBRouter = require("../routes/KBR.js");
+const AWRouter = require("../routes/AWR.js");
 require("./database");
 
 
 // Use ticket routes
 // Use ticket routes
 app.use('/api/tickets', ticketRoutes);
-
+app.use("/api/v1/KB", KBRouter);
+app.use("/api/v1/AW", AWRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
