347396ad791d7ee5799212a976bcef419f2e97f6
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2023-12-27 08:02:25 +0200

    Update messageModel.js

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,20 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const MessageSchema = mongoose.Schema(
-  {
+const messageSchema = mongoose.Schema({
+    conversationId: {
+        type: String,
+    },
+    senderId: {
+        type: String
+    },
     message: {
-      text: { type: String, required: true },
-    },
-    users: Array,
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+        type: String
+    }
+});
 
-module.exports = mongoose.model("Messages", MessageSchema);
+const Messages = mongoose.model('Message', messageSchema);
+
+module.exports = Messages;
