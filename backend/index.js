264462accb8d82da2421d6c705d1d946661d9fa7
LandyPK <100614589+LandyPK@users.noreply.github.com>
2023-12-27 05:56:43 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,72 @@
 const express = require('express');
+const cookieParser=require('cookie-parser');
 const mongoose = require('mongoose');
+const socket = require("socket.io");
+require("dotenv").config();
+const authenticationMiddleware = require("./Middleware/authenticationMiddleware.js");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-const userSchema = require('./models/userSchema'); // Import the User schema
-const ticketRoutes = require('./routes/ticketRoutes');  
+app.use(express.urlencoded({ extended: false }));
+const userRoutes = require('./routes/auth');
+const ticketRoutes = require('./routes/ticketRoutes');
+const recoveryRoutes = require('./routes/recoveryRoutes');
+const Workflow = require('./routes/workflowRoute');
+const messageRoutes = require('./routes/MessageRoute');
+const chatRoute = require('./routes/ChatRoute');
+const baseRoutes = require('./routes/KBR');
 const agentRoutes = require('./routes/agentRoutes');  
-const KBRouter = require("../routes/KBR.js");
-const AWRouter = require("../routes/AWR.js");
+const userSchema = require('./models/userSchema');
+app.use(cors({
+  origin: 'http://localhost:5173'
+  , withCredentials: true  // Replace with the URL of your frontend
+}));
+// Set the base URL for all Axios requests
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+
+app.use('/api/users/', userRoutes);
+app.use(authenticationMiddleware);
+app.use("/api/messages", messageRoutes);
+app.use("/api/chat", chatRoute);
+
+
 require("./database");
 
-
-// Use ticket routes
-// Use ticket routes
+app.use('/recovery', recoveryRoutes);
+app.use('/api/workflow/', Workflow);
 app.use('/api/tickets', ticketRoutes);
-app.use("/api/v1/KB", KBRouter);
-app.use("/api/v1/AW", AWRouter);
+app.use('/api/base', baseRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Import the performBackupAndRestore function
+const { performBackupAndRestore } = require('./backup');
+
+
+// Schedule the backup and restore operation
+// Replace the existing import statement
+
+// Schedule the backup and restore operation
+ 
+
+// The rest of your code remains unchanged
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
 });
+
+
+
