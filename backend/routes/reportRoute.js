06e7f549abc693f3004157f9e7400b85744a5a4a
Rena65 <117449034+Rena65@users.noreply.github.com>
2023-12-27 08:15:22 +0200

    Update reportRoute.js

--- backend/routes/reportRoute.js
+++ backend/routes/reportRoute.js
@@ -7,5 +7,10 @@ router.get("/GetTotalTickets", ReportController.GetTotalTickets);
 router.get("/ticketStatus/:id", ReportController.GetOneTicketStatus);
 router.get("/agentsPerformance", ReportController.GetAllAgentsPerformance);
 router.get("/agentPerformance/:id", ReportController.GetOneAgentPerformance);
+router.get("/generateReports", ReportController.generateReports);
+router.get("/getTicketsCountByDate", ReportController.getTicketsCountByDate);
+router.get("/getTicketsCountBySubIssue", ReportController.getTicketsCountBySubIssue);
+router.get("/getTicketsCountBySubIssueAndDate", ReportController.getTicketsCountBySubIssue_AndDate);
+router.get("/getSpecificTicketsCountByDate", ReportController.get_specific_tickets_CountByDate);
 
 module.exports = router;
