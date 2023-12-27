476f00b21af8c619ed58f722361dca0269e7f94a
Farah-Tarek <134189708+Farah-Tarek@users.noreply.github.com>
2023-12-27 06:25:35 +0200

    Delete backend/routes/AWR.js

--- backend/routes/AWR.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-require("../models/tickets.js");
-require("../models/sub_issues.js");
-require("../models/workflowSchema.js");
-const AWController = require('../controllers/AWC.js');
-router.get("/Solution", AWController.GetSolution);   
-module.exports = router; // ! Don't forget to export the router
