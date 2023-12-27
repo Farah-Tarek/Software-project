0aabf3685f689ad0770eacca5b7d5447cadbdcb7
LandyPK <100614589+LandyPK@users.noreply.github.com>
2023-12-27 05:53:21 +0200

    Update KB.js

--- backend/models/KB.js
+++ backend/models/KB.js
@@ -3,18 +3,26 @@ const Schema = mongoose.Schema
 
 
     const KnowledgeSchema = new Schema({
-    title: { type: String, 
-        required: true },   
+        BaseId: { type: Number, 
+            required: true }, 
+   
+        title: { type: String, 
+        required: true }, 
+        
+        
     CategoryType: {
         type: String,
         enum: ['Software', 'Hardware', 'Network'],
         required: true
         },
+
   Questions: { type: String, 
                 required: true },
+
+                
      Solutions: { type: String,
          required: true },
           
     })
-    const KnowledgeBase = mongoose.model('knowledgebases', KnowledgeSchema);
+    const KnowledgeBase = mongoose.model('kbs', KnowledgeSchema);
     module.exports = KnowledgeBase;
