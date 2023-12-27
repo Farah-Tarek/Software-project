1afbbe9bc74d4cf79929bab407ae52367ebb8712
LandyPK <100614589+LandyPK@users.noreply.github.com>
2023-12-27 06:08:06 +0200

    Update report_schema.js

--- backend/models/report_schema.js
+++ backend/models/report_schema.js
@@ -1,76 +1,48 @@
 const mongoose = require('mongoose');
-//Import the Ticket model
-const Ticket = require('./tickets'); 
-const user = require('./userSchema'); 
-const agent = require('./agent_schema'); 
+const Ticket = require('./tickets');
+const User = require('./userSchema');
+const Agent = require('./agent_schema');
 
-//Import the Automated model model
-//const Automated = require('./Automated'); 
-const Schema = mongoose.Schema
-
+const Schema = mongoose.Schema;
 
 const ReportSchema = new Schema({
+  ResolutionTime: {
+    type: Date,
+  },
+  TicketStatus: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Ticket",
+    required: true,
+    default: null,
+  },
+  UserRatings: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Agent",
+    required: true,
+  },
+});
 
+ReportSchema.pre('save', async function (next) {
+  try {
+    const ticket = await Ticket.findById(this.TicketStatus);
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
 
-    ResolutionTime: 
-    {
-        type: Date,
+    this.TicketStatus = ticket.TicketStatus;
 
-    },
+    const agent = await Agent.findById(this.UserRatings);
+    if (!agent) {
+      throw new Error('Agent not found');
+    }
 
+    this.UserRatings = agent.UserRatings;
 
-  Ticketstatus: 
-    {
-        type: String,
+    // Assuming ResolutionTime is not a reference to another model
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-            type: mongoose.Schema.Types.ObjectId(),
-                ref: "tickets",
-                required: true,
-                default : open,
-                select: 'status',
-            
-        required: true,
-
-    },
-
-
-   AgentPerformance: 
-
-    {
-        
-        type: Number,
-
-            type: mongoose.Schema.Types.ObjectId(),
-            ref: "agent_schema",  // ! check reference  mn meen ticket or automated workflow
-            required: true,
-            select: 'rating', 
-
-        required: true,
-        
-    },
-
-
-
-
-   UserRatings:
-
-    {
-
-          type: Number,
-
-            type: mongoose.Schema.Types.ObjectId(),
-            ref: "agent_schema",  // ! check reference  mn meen ticket or agent
-            required: true,
-            select: 'Rating', 
-
-        required: true,
-
-    },
-
-
-
-
-})
-
-    module.exports = mongoose.model('Report', ReportSchema);
-    module.exports.Schema = ReportSchema;   
+module.exports = mongoose.model('Report', ReportSchema);
