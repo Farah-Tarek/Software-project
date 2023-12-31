26e863ef8cbe812cb87129d27dde49740f5579ad
Karim-Ahmed-03 <134629258+Karim-Ahmed-03@users.noreply.github.com>
2023-12-31 02:38:26 +0200

    Add files via upload

--- backend/routes/reportRoute.js
+++ backend/routes/reportRoute.js
@@ -1,16 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const ReportController = require("../controllers/reportControllers");
+const reportController = require('../controllers/reportController');
 
-router.get("/ticketStatus", ReportController.GetAllTicketStatus);
-router.get("/GetTotalTickets", ReportController.GetTotalTickets);
-router.get("/ticketStatus/:id", ReportController.GetOneTicketStatus);
-router.get("/agentsPerformance", ReportController.GetAllAgentsPerformance);
-router.get("/agentPerformance/:id", ReportController.GetOneAgentPerformance);
-router.get("/generateReports", ReportController.generateReports);
-router.get("/getTicketsCountByDate", ReportController.getTicketsCountByDate);
-router.get("/getTicketsCountBySubIssue", ReportController.getTicketsCountBySubIssue);
-router.get("/getTicketsCountBySubIssueAndDate", ReportController.getTicketsCountBySubIssue_AndDate);
-router.get("/getSpecificTicketsCountByDate", ReportController.get_specific_tickets_CountByDate);
+router.get('/generate-reports', reportController.generateReports);
 
 module.exports = router;
